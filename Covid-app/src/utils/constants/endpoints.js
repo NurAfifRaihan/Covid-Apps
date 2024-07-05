c1e32d6435eb7e5a9b953cec0d25d5e161550538
NurAfifRaihan <114470342+NurAfifRaihan@users.noreply.github.com>
2024-07-06 00:48:31 +0700

    Covid-Apps

--- Covid-app/src/utils/constants/endpoints.js
+++ Covid-app/src/utils/constants/endpoints.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://newsapi.org/v2";
-const API_KEY = import.meta.env.VITE_API_KEY;
+const API_KEY = import.meta.env.NEWS_API_KEY;
 
 const ENDPOINTS = {
     NEWS : `${BASE_URL}/everything?q=keyword&apiKey=${API_KEY}`
