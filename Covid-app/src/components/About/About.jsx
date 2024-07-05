80917d27dae403cc28295447b45b9d278b1e7fff
NurAfifRaihan <114470342+NurAfifRaihan@users.noreply.github.com>
2024-07-06 01:29:05 +0700

    Covid-Apps

--- Covid-app/src/components/About/About.jsx
+++ Covid-app/src/components/About/About.jsx
@@ -1,42 +1,50 @@
 import { useState, useEffect } from 'react';
 import StyledAbout from './About.module';
-import ENDPOINTS from '../../utils/constants/endpoints';
 
-function Berita () {
+function Berita() {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchArticles = async () => {
       try {
-        const response = await fetch(ENDPOINTS.NEWS);
+        const apiKey = 'hpqpqY8RkrPRulWKdXP5vHKmtuGu93Mje7KtW20GXMcHJMP1'; // Ganti dengan API key Anda dari Currents
+        const response = await fetch(`https://api.currentsapi.services/v1/latest-news?apiKey=${apiKey}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch news articles');
+        }
         const data = await response.json();
-        setArticles(data.articles);
+        setArticles(data.news);
         setLoading(false);
       } catch (error) {
-        console.error('Error fetching the news articles:', error);
+        console.error('Error fetching news articles:', error);
         setLoading(false);
+        setError('Failed to fetch news articles');
       }
     };
 
     fetchArticles();
   }, []);
 
-  return ( 
+  return (
     <StyledAbout>
-    <div className="container">
-      <div className="header">Random News in the World</div>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        articles.map((article, index) => (
-          <div key={index} className="article">
-            <div className="title">{article.title}</div>
-            <div className="description">{article.description}</div>
-          </div>
-        ))
-      )}
-    </div>
+      <div className="container">
+        <div className="header">Random News in the World</div>
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>Error: {error}</div>
+        ) : (
+          articles.map((article, index) => (
+            <div key={index} className="article">
+              <div className="title">{article.title}</div>
+              <div className="description">{article.description}</div>
+              <div className="source">{article.author}</div>
+            </div>
+          ))
+        )}
+      </div>
     </StyledAbout>
   );
 }
